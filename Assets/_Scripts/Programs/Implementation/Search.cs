960165aa7fbd70c4d79cfa75367991877dce5dd6
Matheus <temdisponivel@gmail.com>
2016-11-27 22:41:54 -0200

    Add search all files to search program

--- Assets/_Scripts/Programs/Implementation/Search.cs
+++ Assets/_Scripts/Programs/Implementation/Search.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using Hash17.Blackboard_;
+using Hash17.Files;
 using Hash17.Terminal_;
 
 namespace Hash17.Programs.Implementation
@@ -14,31 +15,56 @@ namespace Hash17.Programs.Implementation
         protected override IEnumerator InnerExecute()
         {
             ProgramParameter.Param param;
-            if (Parameters.TryGetParam("", out param))
+            bool all = false;
+            if (!(Parameters.TryGetParam("", out param) || (all = Parameters.TryGetParam("all", out param))) || string.IsNullOrEmpty(param.Value))
             {
-                if (string.IsNullOrEmpty(param.Value))
+                ShowHelp();
+                yield break;
+            }
+
+            var terms = param.Value.Split(';');
+
+            List<File> files;
+            if (all)
+            {
+                files = new List<File>(FileSystem.Instance.Files);
+                Directory root = FileSystem.Instance;
+                var toSee = new List<Directory>();
+                toSee.Add(root);
+                for (int j = 0; j < toSee.Count; j++)
                 {
-                    ShowHelp();
-                    yield break;
-                }
-
-                var terms = param.Value.Split(';');
-                var files = Blackboard.Instance.FileSystem.CurrentDirectory.Files;
-
-                Terminal.Instance.ShowText("Terms found in:");
-
-                for (int i = 0; i < files.Count; i++)
-                {
-                    for (int j = 0; j < terms.Length; j++)
+                    root = toSee[j];
+                    files.AddRange(root.Files);
+                    for (int i = 0; i < root.Childs.Count; i++)
                     {
-                        if (!files[i].Content.Contains(terms[j]))
-                        {
-                            continue;
-                        }
-
-                        Terminal.Instance.ShowText(string.Format("{0} - {1}: {2}", files[i].Name, files[i].Directory.Path, files[i].Content));
+                        toSee.Add(root.Childs[i]);
                     }
                 }
+            }
+            else
+            {
+                files = Blackboard.Instance.FileSystem.CurrentDirectory.Files;
+            }
+
+            Terminal.Instance.ShowText("Terms found in:");
+
+            for (int i = 0; i < files.Count; i++)
+            {
+                var currentFile = files[i];
+
+                var content = currentFile.Content.ToLower();
+
+                if (!terms.Any(t => content.Contains(t)))
+                    continue;
+
+                content = currentFile.Content;
+
+                for (int j = 0; j < terms.Length; j++)
+                {
+                    content = content.Replace(terms[j], string.Format("[b][i]{0}[/i][/b]", terms[j]));
+                }
+
+                Terminal.Instance.ShowText(string.Format("{0} - {1}: {2}", files[i].Name, files[i].Directory.Path, content));
             }
         }
     }
