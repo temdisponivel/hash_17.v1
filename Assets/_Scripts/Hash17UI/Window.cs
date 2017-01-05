05b8e21f3a7dda3a28256956d7d1ef48fd94b274
Matheus <temdisponivel@gmail.com>
2017-01-05 02:51:58 -0200

    Make both scrool views on Window's content work

--- Assets/_Scripts/Hash17UI/Window.cs
+++ Assets/_Scripts/Hash17UI/Window.cs
@@ -159,6 +159,7 @@ public class Window : MonoBehaviour
     {
         _initialScale = transform.localScale;
         _content = new UIWidget();
+        TitleLabel.SetupWithHash17Settings();
     }
 
     void Start()
